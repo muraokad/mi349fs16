3efdb75d0076da8e71ea81687b0492d21ac88f6a
muraokad <muraokad@msu.edu>
2016-10-12 19:44:02 -0400

    Added Week 5

---
+++ Week5/selectors-demo/js/index.js
@@ -0,0 +1,6 @@
+
+var mast = document.getElementsByClassName("highlight");
+mast[0].innerHTML = mast[0].innerHTML + "Test";
+
+var idMast = document.getElementById("specific");
+idMast.innerHTML = idMast.innerHTML + "Test";
